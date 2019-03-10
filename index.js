0d4803a3aaeea1942511b34557047732ea0e0f7b
Pamela Frieze <pfrieze@gmail.com>
2019-03-10 05:33:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function increaseRankBy(n) {
   parseInt(ranks)
   
   for (let i = 0; i < lis.length; i++) {
-  lis[i].innerHTML = (i + 1).toString();
+  lis[i].innerHTML = (i +n).toString();
  }
   
 }
