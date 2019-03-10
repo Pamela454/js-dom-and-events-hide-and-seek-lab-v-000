b62d6de5487dda913a311374c6c6a5d0c0d208ec
Pamela Frieze <pfrieze@gmail.com>
2019-03-10 03:05:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-function getFirstSelector(selector) {
-  filter(selector) {
-    if ( === selector) {
-      console.log(user.firstName);
-    }
+function getFirstSelector( pass ) {
+  return document.querySelector( pass )
+}
+
+function nestedTarget() {
+  return document.getElementById('app').querySelector( '#nested .target' )
+}
+
+function deepestChild() {
+  
+}
+
+function increaseRankBy(n) {
+  const ranks = document.querySelectorAll( '.ranked-list' )
+  parseInt(ranks)
+  for (const i of ranks) {
+  console.log(i + n);
   }
 }
